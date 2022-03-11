b4c9b005d376accd3d19123fb18cb2a9acf4c0b5
João Lucas Abreu <78802497+JoaoLucasAbreu@users.noreply.github.com>
2022-03-11 12:21:05 -0300

    Primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lista lista = new Lista();
+	
+		lista.inserir("joão", 1900191);
+		lista.inserir("jonas", 1123221);
+		
+		lista.imprimir();
+		
+		
+	}
+
+}
